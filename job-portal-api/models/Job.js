6468cdee99141bf6eaa02f44a4a3360780cce466
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-06-04 02:48:57 +0000

    architecture  setup complelty and User model is complete

---
+++ job-portal-api/models/Job.js
@@ -0,0 +1,6 @@
+const mongoose = required ('mongoose');
+const bcrypt = required ('bcrytptjs');
+
+const jobSchema = new mongoose.Schema({
+    
+});
---
+++ job-portal-api/models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "The username is required"],
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, "The emil is required"],
+    trim: true,
+    unique: true,
+    lowercase: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    message: "The password is required",
+  },
+
+  role: {
+    type: String,
+    enum: ["admin", "recruiter", "candidate"],
+    default: "admin",
+    trim: true,
+    required: true,
+  },
+
+  createAt: {
+    Date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+});
+
+//Hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
