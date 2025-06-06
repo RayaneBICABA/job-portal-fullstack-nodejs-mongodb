4033898529b5bff577eb7c95a7816222591fffd5
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-06-06 17:25:12 +0000

    Controllers: Completly setup and formatted

--- job-portal-api/controllers/JobController.js
+++ job-portal-api/controllers/JobController.js
@@ -2,53 +2,62 @@ const Job = require("../models/Job");
 
 // Create
 exports.createJob = async (req, res) => {
-    const { title, description, company, location, salary, createdBy } = req.body;
-    try {
-        const newJob = new Job({ title, description, company, location, salary, createdBy });
-        const savedJob = await newJob.save();
-        res.status(201).json(savedJob);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
+  const { title, description, company, location, salary, createdBy } = req.body;
+  try {
+    const newJob = new Job({
+      title,
+      description,
+      company,
+      location,
+      salary,
+      createdBy,
+    });
+    const savedJob = await newJob.save();
+    res.status(201).json(savedJob);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 // Read All
 exports.getAllJobs = async (req, res) => {
-    try {
-        const jobs = await Job.find();
-        res.json(jobs);
-    } catch (err) {
-        res.status(500).json({ message: "Server error" });
-    }
+  try {
+    const jobs = await Job.find();
+    res.json(jobs);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 // Read One
 exports.getJobById = async (req, res) => {
-    try {
-        const job = await Job.findById(req.params.id);
-        if (!job) return res.status(404).json({ message: "Job not found" });
-        res.json(job);
-    } catch (err) {
-        res.status(500).json({ message: "Server error" });
-    }
+  try {
+    const job = await Job.findById(req.params.id);
+    if (!job) return res.status(404).json({ message: "Job not found" });
+    res.json(job);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 // Update
 exports.updateJob = async (req, res) => {
-    try {
-        const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.json(updatedJob);
-    } catch (err) {
-        res.status(500).json({ message: "Server error" });
-    }
+  try {
+    const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updatedJob);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 // Delete
 exports.deleteJob = async (req, res) => {
-    try {
-        await Job.findByIdAndDelete(req.params.id);
-        res.json({ message: "Job deleted successfully" });
-    } catch (err) {
-        res.status(500).json({ message: "Server error" });
-    }
+  try {
+    await Job.findByIdAndDelete(req.params.id);
+    res.json({ message: "Job deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
